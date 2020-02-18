7a6b21dc0580847716508b1c27b0c366de99c1a8
Jeroen Claessens <jclaessens97@hotmail.com>
2020-02-18 19:53:29 +0100

    Pizza class

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('yolo')
+class Pizza:
+  def __init__(self, pizzaType, number_of_slices):
+    self.pizzaType = pizzaType
+    self.number_of_slices = number_of_slices
+
+
+def main():
+  pizza = Pizza(0, 3)
+  pass
+
+if (__name__ == '__main__'):
+  main()
